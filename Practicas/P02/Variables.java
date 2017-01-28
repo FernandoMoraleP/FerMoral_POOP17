60de123797d79cc6a71b03a3abe8cb7a8616a798
Fernando Morales <derox59@hotmail.com>
2017-01-28 01:13:34 -0600

    My 2th practice

---
+++ Practicas/P02/Variables.java
@@ -0,0 +1,20 @@
+public class Variables{
+	static String var;
+	public static void main(String[] args){
+		byte var1 = 127;
+		short var2 = 32767;
+		int var3 = 2147483647;
+		long var4 = 9223372036854775807L;
+		char var5 = '2';
+		String var6 = "hola java";
+
+
+		System.out.println("byte var = " + var1);
+		System.out.println("short var = " + var2);
+		System.out.println("int var = " + var3);
+		System.out.println("long var = " + var4);
+		System.out.println("char var = " + var5);
+		System.out.println("string var = " + var);
+		System.out.println("string var = " + var6);
+	}
+}
